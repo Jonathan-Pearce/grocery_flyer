c090957a1c609ac8177d8ff6188c482edffa6bfd
Jonathan Pearce <Jonathan-Pearce@users.noreply.github.com>
2026-05-11 20:34:13 +0000

    docker updates

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import DealsView from '@/views/DealsView.vue'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: HomeView, name: 'home' },
     { path: '/deals', component: DealsView, name: 'deals' }
